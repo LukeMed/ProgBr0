e66f71c70d613a3b6207eead4b49ad45c2732841
LukeMed <luke.lam1988@gmail.com>
2024-11-15 20:44:24 -0300

    Atualização2 de 15/11/2024

---
+++ index.js
@@ -0,0 +1,8 @@
+const refeição = " a refeição é"
+let produtos = [
+    [" Açucar", " Café", " Leite", " Pão"], 
+    ["Arroz", "Feijão", "Frango", "Salada"]
+
+]
+
+console.log("Pela manhã" + refeição + produtos[0][0] + produtos[0][1] + produtos[0][2] + produtos[0][3])
